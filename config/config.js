0adb31a30aa94e82af7bc25a2559b0121c9e3548
Shawn A. R <sarsa.web@gmail.com>
2021-02-12 17:42:23 -0500

    working on DB

--- config/config.js
+++ config/config.js
@@ -15,9 +15,9 @@ module.exports = {
     "database": "database_test",
     "host": "127.0.0.1",
     "dialect": "mysql"
-  },
-  "production": {
-    "use_env_variable": "JAWSDB_URL",
-    "dialect": "mysql"
   }
+  // "production": {
+  //   "use_env_variable": "JAWSDB_URL",
+  //   "dialect": "mysql"
+  // }
 }
