f9c0ee2c42947eb3767f6b656c5c22b480d0e3e1
Shawn A. R <sarsa.web@gmail.com>
2021-02-16 13:21:59 -0500

    landing navigation set correctly

---
+++ client/src/components/landingPage_Top.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./landingPage_Top.css";
+
+function landingPage_Top() {
+    return (
+        <div className="top">
+            <h1>GAMING ARE US</h1>
+        </div>
+    )
+}
+
+export default landingPage_Top;
--- client/src/pages/landingPage.js
+++ client/src/pages/landingPage.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Top from '../components/landingPage_Top';
+import './landingPage.css';
 // import "../styles/Intro.css";
 // import { Button } from "react-bootstrap";
 
@@ -7,6 +9,7 @@ import React from 'react';
 const landingPage = () => {
     return (
         <div>
+        <Top />
         <div className="introDiv">
             <h1>Landing Page</h1>
         </div>
