2065e2d66154181b6b63d18507a50dcd5467d554
Shawn A. R <sarsa.web@gmail.com>
2021-02-19 00:18:51 -0500

    added dependencies

---
+++ client/src/components/landingPage_Bottom/landingPage_Bottom.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./landingPage_Bottom.css";
+
+function landingPage_Bottom() {
+    return (
+        <div className="main_bottom">
+            <h3>All copyrights and trademarks of the character images used belong to thier respective owners.
+            This project is for learning purposes and I don't recieve any revenue from this site.</h3>
+        </div>
+    )
+}
+
+export default landingPage_Bottom;
--- client/src/pages/landingPage/landingPage.js
+++ client/src/pages/landingPage/landingPage.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Top from '../../components/landingPage_Top/landingPage_Top';
 import Main from '../../components/landingPage_Main/landingPage_Main';
+import Bottom from '../../components/landingPage_Bottom/landingPage_Bottom';
 import './landingPage.css';
 // import "../styles/Intro.css";
 // import { Button } from "react-bootstrap";
@@ -12,6 +13,7 @@ const landingPage = () => {
         <div>
         <Top />
         <Main />
+        <Bottom />
         </div>
     );
 };
